909d8c5b068a00cee6528e4e3f0984eef6a6f773
KMNK <kmnknmk+com-github@gmail.com>
2022-06-17 22:51:22 +0900

    update

--- denops/helloworld/main.ts
+++ denops/helloworld/main.ts
@@ -1,6 +1,12 @@
 import { Denops } from "https://deno.land/x/denops_std@v3.3.2/mod.ts";
+import { ensureString } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
-  // Plugin program starts from here
-  console.log("Hello Denops!");
+  denops.dispatcher = {
+    async echo(text: unknown): Promise<unknown> {
+      // assure `text` is string type.
+      ensureString(text);
+      return await Promise.resolve(text);
+    },
+  };
 };

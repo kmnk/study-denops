ddcf93055a604fe371c13921a82737ff0b7523a9
KMNK <kmnknmk+com-github@gmail.com>
2022-06-17 22:48:03 +0900

    Fist commit

---
+++ denops/helloworld/main.ts
@@ -0,0 +1,6 @@
+import { Denops } from "https://deno.land/x/denops_std@v3.3.2/mod.ts";
+
+export async function main(denops: Denops): Promise<void> {
+  // Plugin program starts from here
+  console.log("Hello Denops!");
+};
